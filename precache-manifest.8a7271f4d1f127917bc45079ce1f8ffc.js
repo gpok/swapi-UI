79ad7293814946490093d54abe7bd5adc05613a5
Grzegorz Pokorny <grzegorz.pokorny.94@gmail.home>
2020-05-28 01:55:07 +0200

    Updates

---
+++ precache-manifest.8a7271f4d1f127917bc45079ce1f8ffc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd508a7b90bf301e816592e2bb13fedb",
+    "url": "/swapi-UI/index.html"
+  },
+  {
+    "revision": "35ea160998fc713bd045",
+    "url": "/swapi-UI/static/css/main.00855698.chunk.css"
+  },
+  {
+    "revision": "953742fed0f6d23a357d",
+    "url": "/swapi-UI/static/js/2.7e8fced2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/swapi-UI/static/js/2.7e8fced2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35ea160998fc713bd045",
+    "url": "/swapi-UI/static/js/main.3999cc17.chunk.js"
+  },
+  {
+    "revision": "341ee7b29c8d8dbe073e",
+    "url": "/swapi-UI/static/js/runtime-main.9929a8b4.js"
+  },
+  {
+    "revision": "79e78bde4e053419076ba797c2f359e1",
+    "url": "/swapi-UI/static/media/holocron.79e78bde.png"
+  },
+  {
+    "revision": "5366247df42d4a919acf0a8ea34e0283",
+    "url": "/swapi-UI/static/media/icon.5366247d.gif"
+  },
+  {
+    "revision": "9f9cc56f4ff93729840c65bd5cc2916c",
+    "url": "/swapi-UI/static/media/logo_small.9f9cc56f.png"
+  },
+  {
+    "revision": "5f909bb037431115b2225e40cd08b202",
+    "url": "/swapi-UI/static/media/placeholderCharacter.5f909bb0.png"
+  },
+  {
+    "revision": "9cc316799074c8098646af205ed06d66",
+    "url": "/swapi-UI/static/media/placeholderPlanet.9cc31679.png"
+  },
+  {
+    "revision": "3c0180bde0cb0b8e4480f4e773cd0319",
+    "url": "/swapi-UI/static/media/placeholderSpaceship.3c0180bd.png"
+  },
+  {
+    "revision": "423bb5af1c173553dcebb662ee5e9606",
+    "url": "/swapi-UI/static/media/planet.423bb5af.png"
+  },
+  {
+    "revision": "511c50340d1eea4a2cb99ce15f8794f7",
+    "url": "/swapi-UI/static/media/stars.511c5034.jpg"
+  }
+]);
